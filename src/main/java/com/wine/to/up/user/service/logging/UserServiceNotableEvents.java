846dfdae2b7e459d14544fe21450acb7935562bb
саша <>
2020-09-23 13:47:32 +0300

    Add project

---
+++ src/main/java/com/wine/to/up/user/service/logging/UserServiceNotableEvents.java
@@ -0,0 +1,26 @@
+package com.wine.to.up.user.service.logging;
+
+import com.wine.to.up.commonlib.logging.NotableEvent;
+
+public enum UserServiceNotableEvents implements NotableEvent {
+    I_KAFKA_SEND_MESSAGE_SUCCESS("Kafka send message: {}"),
+    I_CONTROLLER_RECEIVED_MESSAGE("Message: {}"),
+    W_SOME_WARN_EVENT("Warn situation. Description: {}");
+
+    private final String template;
+
+    UserServiceNotableEvents(String template) {
+        this.template = template;
+    }
+
+    public String getTemplate() {
+        return template;
+    }
+
+    @Override
+    public String getName() {
+        return name();
+    }
+
+
+}
